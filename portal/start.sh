4e233488ea3efef9b5a6cbc2c71cb26716f40442
Ed Singleton <singletoned@gmail.com>
2023-03-14 08:27:30 +0000

    Delete some unused files

--- portal/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-
-python manage.py migrate --noinput
-watchmedo auto-restart --directory=./  --pattern=""*.py"" --recursive -- waitress-serve --port=$PORT help_to_heat.wsgi:application
