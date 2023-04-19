8a2d6de2bcd92d3b2f26a0daeceb9c4cd702c408
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-04-19 15:05:52 +0100

    Updated supplier_and_user_management_views.py to handle team leader and updated some supplier admin pages

--- portal/help_to_heat/ecoplus/supplier_and_user_management_views.py
+++ portal/help_to_heat/ecoplus/supplier_and_user_management_views.py
@@ -27,6 +27,9 @@ def edit_supplier_view(request, supplier_id):
 def change_supplier_disabled_status_view(request, supplier_id):
     if request.method == "GET":
         supplier = models.Supplier.objects.get(pk=supplier_id)
+        return render(request, "supplier-admin/disable-supplier-confirmation.html", {"supplier": supplier})
+    else:
+        supplier = models.Supplier.objects.get(pk=supplier_id)
         supplier.is_disabled = not supplier.is_disabled
         supplier.save()
         return redirect("homepage")
@@ -35,7 +38,7 @@ def change_supplier_disabled_status_view(request, supplier_id):
 def supplier_team_leads_view(request, supplier_id):
     if request.method == "GET":
         supplier = models.Supplier.objects.get(pk=supplier_id)
-        users = supplier.user_set.all()
+        users = supplier.user_set.filter(is_team_leader=True).all()
         return render(request, "supplier-admin/supplier-team-lead-list.html", {"users": users, "supplier": supplier})
 
 
@@ -80,3 +83,46 @@ def supplier_team_leads_remove_view(request, supplier_id, user_id):
         user.supplier = None
         user.save()
         return redirect("supplier-team-leads", supplier.id)
+
+
+def team_member_add_role_view(request, supplier_id):
+    if request.method == "GET":
+        return render(request, "team-leader/add-user-role-select.html", {"supplier_id": supplier_id})
+    else:
+        user_role = request.POST.get("team-role")
+        return redirect("add-user-details-select", user_role=user_role, supplier_id=supplier_id)
+
+
+def team_member_add_details_view(request, supplier_id, user_role):
+    if request.method == "GET":
+        return render(request, "team-leader/add-user-details.html", {"user_role":user_role, "supplier_id":supplier_id})
+    else:
+        supplier = models.Supplier.objects.get(pk=supplier_id)
+        user_name = request.POST.get("user-name")
+        user_email = request.POST.get("user-email")
+
+        user, _ = models.User.objects.get_or_create(email=user_email)
+        user.full_name = user_name
+        if user_role == "team-leader":
+            user.is_team_leader = True
+        else:
+            user.is_team_member = True
+        user.supplier = supplier
+        user.save()
+        return redirect("homepage")
+
+
+def team_member_details_view(request, supplier_id, user_id):
+    user = models.User.objects.get(pk=user_id)
+    return render(request, "team-leader/view-user-details.html", {"supplier_id": supplier_id, "user": user})
+
+
+def team_member_remove_view(request, supplier_id, user_id):
+    if request.method == "GET":
+        user = models.User.objects.get(pk=user_id)
+        return render(request, "team-leader/remove-user.html", {"supplier_id": supplier_id, "user": user})
+    else:
+        user = models.User.objects.get(pk=user_id)
+        user.is_active = False
+        user.save()
+        return redirect("homepage")
