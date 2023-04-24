ce31a7b5bd29c1188d84ad376f606fafc243f1d3
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-04-24 16:26:11 +0100

    Correct test run

--- portal/tests/test_registration.py
+++ portal/tests/test_registration.py
@@ -7,4 +7,4 @@ TEST_SERVER_URL = "http://help-to-heat-testserver/"
 def test_registration():
     client = testino.WSGIAgent(wsgi.application, TEST_SERVER_URL)
     page = client.get("/")
-    assert page.status_code == 200
+    assert page.status_code == 302
