53a98d2b489b62464974407d4c52d0b3fc4017bf
Antonio Lopes <111270319+aialopes@users.noreply.github.com>
2023-04-28 09:35:05 +0100

    Copy updates for "Available Schemes" (#95)

--- web/src/components/screens/ListOfElegibleSchemes.tsx
+++ web/src/components/screens/ListOfElegibleSchemes.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { useTranslation } from 'next-i18next'
 import { Button } from '@/components/ui/Button'
+import * as GovUK from 'govuk-react'
+
+import RelatedItems from '../ui/RelatedItems'
 
 export const ListOfElegibleSchemes = (props: { onSubmit: () => void }) => {
   const { t } = useTranslation(['screens'])
   return (
     <div>
-      <h1>{t('ListOfElegibleSchemes.title')}</h1>
+      <GovUK.Heading size="L">{t('ListOfElegibleSchemes.title')}</GovUK.Heading>
+      <GovUK.Paragraph>{t('ListOfElegibleSchemes.subTitle') as string}</GovUK.Paragraph>
+
+      <RelatedItems>
+        <GovUK.Caption size="M">
+          {t('ListOfElegibleSchemes.suplier.header')}
+        </GovUK.Caption>
+        <GovUK.Heading size="M">{t('ListOfElegibleSchemes.suplier.title')}</GovUK.Heading>
+        <GovUK.Paragraph>
+          {t('ListOfElegibleSchemes.suplier.body1') as string}
+        </GovUK.Paragraph>
+        <GovUK.Paragraph>
+          {t('ListOfElegibleSchemes.suplier.body2') as string}
+        </GovUK.Paragraph>
+      </RelatedItems>
+
       <Button
         type="button"
         onClick={() => {
@@ -14,7 +32,7 @@ export const ListOfElegibleSchemes = (props: { onSubmit: () => void }) => {
         }}
       >
         {
-          t('continue', {
+          t('create-a-referral', {
             ns: 'common'
           }) as string
         }
---
+++ web/src/components/ui/RelatedItems.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RelatedItems as GovUKRelatedItems } from 'govuk-react'
+import styled, { StyledComponentProps } from 'styled-components'
+
+const RelatedItems = styled(GovUKRelatedItems)`
+  border: 1px solid #d9d9d9;
+  border-top: 7px solid #1d70b8;
+  padding: 16px;
+  margin-bottom: 32px;
+`
+
+export default RelatedItems
