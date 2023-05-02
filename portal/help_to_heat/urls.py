7ef5ba7504f2b7411f83eb837e594d77b9272008
Ed Singleton <singletoned@gmail.com>
2023-05-02 13:52:44 +0100

    Add setting for FRONTDOOR

--- portal/help_to_heat/urls.py
+++ portal/help_to_heat/urls.py
@@ -1,9 +1,21 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from help_to_heat.frontdoor.urls import frontdoor_patterns
 from help_to_heat.portal.urls import api_patterns, portal_patterns
 
-urlpatterns = [
+if settings.SHOW_FRONTDOOR:
+    urlpatterns = [
+        path("", include(frontdoor_patterns)),
+        path("portal/", include(portal_patterns)),
+    ]
+else:
+    urlpatterns = [
+        path("", include(portal_patterns)),
+    ]
+
+
+urlpatterns = urlpatterns + [
     path("admin/", admin.site.urls),
     path("api/", include(api_patterns)),
-    path("portal/", include(portal_patterns)),
 ]
