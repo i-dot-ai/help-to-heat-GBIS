99d7b459ba2316e335e87eabb876486d52fdb548
Ed Singleton <singletoned@gmail.com>
2023-05-03 18:51:42 +0100

    Refactor tests

--- portal/tests/test_frontdoor.py
+++ portal/tests/test_frontdoor.py
@@ -1,15 +1,14 @@
 import unittest
 
-import testino
 from django.conf import settings
-from help_to_heat import wsgi
 
-TEST_SERVER_URL = "http://help-to-heat-testserver/"
+from . import utils
 
 
 @unittest.skipIf(not settings.SHOW_FRONTDOOR, "Frontdoor disabled")
 def test_flow():
-    client = testino.WSGIAgent(wsgi.application, TEST_SERVER_URL)
+    client = utils.get_client()
     page = client.get("/")
+
     assert page.status_code == 200
     assert page.has_one("h1:contains('Homepage')")
