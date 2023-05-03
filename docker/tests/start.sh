8aca50a1c11f7df5f2d99f6f8d9b13c1f4e3ef2e
Ed Singleton <singletoned@gmail.com>
2023-05-03 11:07:59 +0100

    Run tests with and without SHOW_FRONTDOOR

--- docker/tests/start.sh
+++ docker/tests/start.sh
@@ -7,4 +7,14 @@ python manage.py migrate --noinput
 echo
 echo '----------------------------------------------------------------------'
 echo
-nosetests -v ./tests
+
+echo '================================='
+echo "Run tests with FRONTDOOR disabled"
+echo '================================='
+SHOW_FRONTDOOR=False nosetests -v ./tests
+echo
+echo
+echo '================================='
+echo "Run tests with FRONTDOOR enabled"
+echo '================================='
+SHOW_FRONTDOOR=True nosetests -v ./tests
