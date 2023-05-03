b5c2f79ba91bb9cc5cd8adee57b77b630964e4f0
Ed Singleton <singletoned@gmail.com>
2023-05-03 11:07:51 +0100

    Fix portal test

--- portal/tests/test_registration.py
+++ portal/tests/test_registration.py
@@ -1,10 +1,16 @@
 import testino
+from django.conf import settings
 from help_to_heat import wsgi
 
 TEST_SERVER_URL = "http://help-to-heat-testserver/"
 
+if settings.SHOW_FRONTDOOR:
+    url_prefix = "/portal"
+else:
+    url_prefix = ""
+
 
 def test_registration():
     client = testino.WSGIAgent(wsgi.application, TEST_SERVER_URL)
-    page = client.get("/")
+    page = client.get(f"{url_prefix}/")
     assert page.status_code == 302
