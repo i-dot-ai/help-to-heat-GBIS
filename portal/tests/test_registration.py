8d72915cd4fbff607e4745d860f236820a8a915f
Ed Singleton <singletoned@gmail.com>
2023-05-03 18:51:42 +0100

    Refactor tests

--- portal/tests/test_registration.py
+++ portal/tests/test_registration.py
@@ -1,16 +1,7 @@
-import testino
-from django.conf import settings
-from help_to_heat import wsgi
-
-TEST_SERVER_URL = "http://help-to-heat-testserver/"
-
-if settings.SHOW_FRONTDOOR:
-    url_prefix = "/portal"
-else:
-    url_prefix = ""
+from . import utils
 
 
 def test_registration():
-    client = testino.WSGIAgent(wsgi.application, TEST_SERVER_URL)
-    page = client.get(f"{url_prefix}/")
+    client = utils.get_portal_client()
+    page = client.get("/")
     assert page.status_code == 302
