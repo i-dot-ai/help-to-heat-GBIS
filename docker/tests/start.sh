a893aeb6a6050060034e12b7d66db6f315623183
Ed Singleton <singletoned@gmail.com>
2023-05-07 08:41:50 +0100

    Add suppliers in tests

--- docker/tests/start.sh
+++ docker/tests/start.sh
@@ -4,6 +4,9 @@ set -o errexit
 set -o nounset
 
 python manage.py migrate --noinput
+
+python manage.py add_suppliers
+
 echo
 echo '----------------------------------------------------------------------'
 echo
