6b0c6650fe8f8ee9dba482097fae06fa5ec42e74
Ed Singleton <singletoned@gmail.com>
2023-05-10 17:14:13 +0100

    Add simple page view

--- portal/help_to_heat/frontdoor/views.py
+++ portal/help_to_heat/frontdoor/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def homepage_view(request):
     return render(request, template_name="frontdoor/homepage.html")
+
+
+def page_view(request, page_name):
+    return render(request, template_name=f"frontdoor/{page_name}.html")
