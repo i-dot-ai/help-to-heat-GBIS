34ea6e829c967b4a9c2c3c9124248a3b86865c9c
Ed Singleton <singletoned@gmail.com>
2023-05-16 21:58:13 +0100

    Fix SessionSchema

--- portal/help_to_heat/frontdoor/schemas.py
+++ portal/help_to_heat/frontdoor/schemas.py
@@ -33,3 +33,5 @@ class SessionSchema(Schema):
     postcode = fields.String()
     council_tax_band = fields.String(validate=validate.OneOf(council_tax_band_options))
     benefits = fields.String(validate=validate.OneOf(yes_no_options))
+    household_income = fields.String(validate=validate.OneOf(household_income_options))
+    property_type = fields.String(validate=validate.OneOf(property_type_options))
