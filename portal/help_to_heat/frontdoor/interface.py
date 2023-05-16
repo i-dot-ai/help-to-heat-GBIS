69e76e546cd65d5bf582487d884bef5847a30b1c
Ed Singleton <singletoned@gmail.com>
2023-05-16 10:43:58 +0100

    Test interface

---
+++ portal/help_to_heat/frontdoor/interface.py
@@ -0,0 +1,31 @@
+import marshmallow
+from help_to_heat.utils import Entity, Interface, register_event, with_schema
+
+from . import models, schemas
+
+
+class SaveAnswerSchema(marshmallow.Schema):
+    data = marshmallow.fields.Raw()
+    session_id = marshmallow.fields.UUID()
+    page_name = marshmallow.fields.String()
+
+
+class GetAnswerSchema(marshmallow.Schema):
+    session_id = marshmallow.fields.UUID()
+    page_name = marshmallow.fields.String()
+
+
+class Session(Entity):
+    @with_schema(load=SaveAnswerSchema, dump=schemas.SessionSchema)
+    @register_event(models.Event, "Answer saved")
+    def save_answer(self, session_id, page_name, data):
+        answer = models.Answer.objects.create(data=data, session_id=session_id, page_name=page_name)
+        return answer.data
+
+    @with_schema(load=GetAnswerSchema, dump=schemas.SessionSchema)
+    def get_answer(self, session_id, page_name):
+        answer = models.Answer.objects.get(session_id=session_id, page_name=page_name)
+        return answer.data
+
+
+api = Interface(session=Session())
---
+++ portal/tests/test_interface.py
@@ -0,0 +1,15 @@
+import uuid
+
+from help_to_heat.frontdoor import interface
+
+
+def test_answers():
+    session_id = uuid.uuid4()
+    page_name = "flibble"
+    data = {"floob": "blumble", "country": "England"}
+    result = interface.api.session.save_answer(session_id=session_id, page_name=page_name, data=data)
+    expected = {"country": "England"}
+    assert result == expected, (result, expected)
+
+    result = interface.api.session.get_answer(session_id=session_id, page_name=page_name)
+    assert result == expected, (result, expected)
