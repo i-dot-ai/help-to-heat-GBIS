42a4f325d3ce20375d7ecfe292b06d5459824bde
Ed Singleton <singletoned@gmail.com>
2023-05-16 10:29:50 +0100

    Add an Event model

--- portal/help_to_heat/frontdoor/models.py
+++ portal/help_to_heat/frontdoor/models.py
@@ -5,6 +5,11 @@ from django.db import models
 from help_to_heat import utils
 
 
+class Event(utils.TimeStampedModel):
+    name = models.CharField(max_length=256)
+    data = models.JSONField(encoder=DjangoJSONEncoder)
+
+
 class Answer(utils.UUIDPrimaryKeyBase, utils.TimeStampedModel):
     data = models.JSONField(encoder=DjangoJSONEncoder, editable=False)
     page_name = models.CharField(max_length=128, editable=False)
