7b3b57497bcec24898aca55423c54c2971c81c25
Ed Singleton <singletoned@gmail.com>
2023-05-16 16:30:09 +0100

    Factor out PageView

--- portal/help_to_heat/frontdoor/views.py
+++ portal/help_to_heat/frontdoor/views.py
@@ -36,59 +36,52 @@ def homepage_view(request):
     return render(request, template_name="frontdoor/homepage.html", context=context)
 
 
-@register_page("country")
-class CountryView(utils.MethodDispatcher):
+class PageView(utils.MethodDispatcher):
     def get(self, request, session_id, page_name):
-        context = {"country_options": schemas.country_options}
-        return render(request, template_name="frontdoor/country.html", context=context)
+        context = self.get_context(request, session_id, page_name)
+        return render(request, template_name=f"frontdoor/{page_name}.html", context=context)
 
     def post(self, request, session_id, page_name):
-        result = schemas.SessionSchema(unknown=marshmallow.EXCLUDE).load(request.POST)
-        models.Answer.objects.create(data=result, session_id=session_id, page_name=page_name)
-        if result["country"] == "Northern Ireland":
+        data = schemas.SessionSchema(unknown=marshmallow.EXCLUDE).load(request.POST)
+        models.Answer.objects.create(data=data, session_id=session_id, page_name=page_name)
+        return self.handle_post(request, session_id, page_name, data)
+
+    def get_context(self, *args, **kwargs):
+        return {}
+
+    def handle_post(self, request, session_id, page_name, data):
+        next_page_name = page_order[page_order.index(page_name) + 1]
+        return redirect("frontdoor:page", session_id=session_id, page_name=next_page_name)
+
+
+@register_page("country")
+class CountryView(PageView):
+    def get_context(self, *args, **kwargs):
+        return {"country_options": schemas.country_options}
+
+    def handle_post(self, request, session_id, page_name, data):
+        if data["country"] == "Northern Ireland":
             return redirect("frontdoor:page", session_id=session_id, page_name="northern-ireland")
         else:
-            next_page_name = page_order[page_order.index("country") + 1]
+            next_page_name = page_order[page_order.index(page_name) + 1]
             return redirect("frontdoor:page", session_id=session_id, page_name=next_page_name)
 
 
 @register_page("own-property")
-class OwnPropertyView(utils.MethodDispatcher):
-    def get(self, request, session_id, page_name):
-        context = {"own_property_options": schemas.own_property_options}
-        return render(request, template_name="frontdoor/own-property.html", context=context)
-
-    def post(self, request, session_id, page_name):
-        result = schemas.SessionSchema(unknown=marshmallow.EXCLUDE).load(request.POST)
-        models.Answer.objects.create(data=result, session_id=session_id, page_name=page_name)
-        next_page_name = page_order[page_order.index("own-property") + 1]
-        return redirect("frontdoor:page", session_id=session_id, page_name=next_page_name)
+class OwnPropertyView(PageView):
+    def get_context(self, *args, **kwargs):
+        return {"own_property_options": schemas.own_property_options}
 
 
 @register_page("address")
-class AddressView(utils.MethodDispatcher):
-    def get(self, request, session_id, page_name):
-        context = {}
-        return render(request, template_name="frontdoor/address.html", context=context)
-
-    def post(self, request, session_id, page_name):
-        result = schemas.SessionSchema(unknown=marshmallow.EXCLUDE).load(request.POST)
-        models.Answer.objects.create(data=result, session_id=session_id, page_name=page_name)
-        next_page_name = page_order[page_order.index("address") + 1]
-        return redirect("frontdoor:page", session_id=session_id, page_name=next_page_name)
+class AddressView(PageView):
+    pass
 
 
 @register_page("council-tax-band")
-class CouncilTaxBandView(utils.MethodDispatcher):
-    def get(self, request, session_id, page_name):
-        context = {"council_tax_band_options": schemas.council_tax_band_options}
-        return render(request, template_name="frontdoor/council-tax-band.html", context=context)
-
-    def post(self, request, session_id, page_name):
-        result = schemas.SessionSchema(unknown=marshmallow.EXCLUDE).load(request.POST)
-        models.Answer.objects.create(data=result, session_id=session_id, page_name=page_name)
-        next_page_name = page_order[page_order.index("council-tax-band") + 1]
-        return redirect("frontdoor:page", session_id=session_id, page_name=next_page_name)
+class CouncilTaxBandView(PageView):
+    def get_context(self, *args, **kwargs):
+        return {"council_tax_band_options": schemas.council_tax_band_options}
 
 
 def page_view(request, session_id, page_name):
