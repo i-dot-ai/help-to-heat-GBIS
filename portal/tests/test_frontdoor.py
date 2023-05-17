aa9b9d84dfa713b96134c1f4a4538d8e857f20a6
Ed Singleton <singletoned@gmail.com>
2023-05-17 18:32:31 +0100

    Add a no benefits flow

--- portal/tests/test_frontdoor.py
+++ portal/tests/test_frontdoor.py
@@ -147,3 +147,71 @@ def test_back_button():
 
     form = page.get_form()
     assert form["country"] == "England"
+
+
+@unittest.skipIf(not settings.SHOW_FRONTDOOR, "Frontdoor disabled")
+def test_no_benefits_flow():
+    client = utils.get_client()
+    page = client.get("/")
+
+    assert page.status_code == 200
+    assert page.has_one("h1:contains('Get home energy improvements')")
+
+    page = page.click(contains="Start")
+    assert page.status_code == 200
+
+    session_id = page.path.split("/")[1]
+    assert uuid.UUID(session_id)
+
+    _check_page = _make_check_page(session_id)
+
+    form = page.get_form()
+    form["country"] = "England"
+    page = form.submit().follow()
+
+    assert page.has_text("Do you own your property?")
+    page = _check_page(page, "own-property", "own_property", "Yes, I own my property and live in it")
+
+    assert page.has_one("h1:contains('What is the address of your property?')")
+
+    form = page.get_form()
+    form["address_line_1"] = "999 Letsby Avenue"
+    form["postcode"] = "PO99 9PO"
+    page = form.submit().follow()
+
+    data = interface.api.session.get_answer(session_id, page_name="address")
+    assert data["address_line_1"] == "999 Letsby Avenue"
+    assert data["postcode"] == "PO99 9PO"
+
+    assert page.has_one("h1:contains('What is the council tax band of your property?')")
+    page = _check_page(page, "council-tax-band", "council_tax_band", "B")
+
+    assert page.has_one("h1:contains('Is anyone in your household receiving any benefits?')")
+    page = _check_page(page, "benefits", "benefits", "No")
+
+    assert page.has_one("h1:contains('What is your annual household income?')")
+    page = _check_page(page, "household-income", "household_income", "Less than £31,000 a year")
+
+    assert page.has_one("h1:contains('What kind of property do you have?')")
+    page = _check_page(page, "property-type", "property_type", "House")
+
+    assert page.has_one("h1:contains('Number of bedrooms')")
+    page = _check_page(page, "number-of-bedrooms", "number_of_bedrooms", "Two bedrooms")
+
+    assert page.has_one("h1:contains('What kind of walls does your property have?')")
+    page = _check_page(page, "wall-type", "wall_type", "Cavity walls")
+
+    assert page.has_one("h1:contains('Are your walls insulated?')")
+    page = _check_page(page, "wall-insulation", "wall_insulation", "No they are not insulated")
+
+    assert page.has_one("h1:contains('Does this property have a loft?')")
+    page = _check_page(page, "loft", "loft", "No")
+
+    assert page.has_one("h1:contains('Is there access to your loft?')")
+    page = _check_page(page, "loft-access", "loft_access", "Yes, there is access to my loft")
+
+    assert page.has_one("h1:contains('Information based on your answers')")
+    assert page.has_text("Great British Insulation scheme")
+    assert not page.has_text("Energy Company Obligation 4")
+    form = page.get_form()
+    page = form.submit().follow()
