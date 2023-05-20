2a4cbf3335e12b567138d39863937f87e6571612
Ed Singleton <singletoned@gmail.com>
2023-05-20 10:19:43 +0100

    Add mixed referral downloads

--- portal/tests/test_portal.py
+++ portal/tests/test_portal.py
@@ -68,3 +68,67 @@ def test_service_manager_add_supplier():
     page = form.submit().follow()
 
     assert page.has_one(f"""th:contains("{team_lead_name} v2") ~ td:nth-of-type(2):contains("Disabled")""")
+
+
+def test_legacy_download():
+    legacy_data = {
+        "loft": "no",
+        "house": "semi-detached",
+        "walls": "solid",
+        "location": "England",
+        "property": "house",
+        "addressUPRN": "202134001",
+        "housingStatus": "owner",
+        "councilTaxBand": "C",
+        "energySupplier": "Octopus",
+        "propertyHasEpc": "yes",
+        "wallInsulation": "some",
+        "householdIncome": "<£31k",
+        "address.postcode": "W1A 1AA",
+        "numberOfBedrooms": "3+",
+        "receivingBenefits": "yes",
+        "suggestedEPCFound": False,
+        "counciltaxBandsSize": 8,
+        "personalDetails.email": "flampy.flibbleton@example.com",
+        "personalDetails.lastName": "Flampy",
+        "personalDetails.firstName": "Flibbleton",
+        "personalDetails.phoneNumber": "07950000000",
+        "address.buildingNumberOrName": "63",
+        "propertyEpcDetails.propertyEpcRating": "E",
+        "propertyEpcDetails.propertyEpcDate.day": "01",
+        "propertyEpcDetails.propertyEpcDate.year": "1990",
+        "propertyEpcDetails.propertyEpcDate.month": "01",
+    }
+    anterograde_data = {
+        "benefits": "Yes",
+        "country": "Wales",
+        "property_type": "House",
+        "last_name": "",
+        "email": "",
+        "own_property": "No, I am a tenant",
+        "number_of_bedrooms": "Three or more bedrooms",
+        "council_tax_band": "A",
+        "loft_access": "No, there is no access to my loft",
+        "address_line_1": "5 Sesame Street",
+        "wall_type": "Cavity walls",
+        "wall_insulation": "Some are insulated, some are not",
+        "loft": "No",
+        "first_name": "",
+        "supplier": "Octopus",
+        "contact_number": "",
+        "postcode": "S55 5SS",
+        "household_income": "Less than £31,000 a year",
+    }
+    supplier = models.Supplier.objects.get(name="Octopus")
+    models.Referral.objects.create(supplier=supplier, data=legacy_data)
+    models.Referral.objects.create(supplier=supplier, data=anterograde_data)
+
+    client = utils.get_client()
+    page = utils.login_as_team_leader(client)
+    assert page.has_one("p:contains('Unread leads') ~ p:contains('2')")
+
+    csv_page = page.click(contains="Download latest leads")
+    text = csv_page.content
+    lines = text.splitlines()
+    assert len(lines) == 3
+    assert len(lines[0].split(b",")) == 27, len(lines[0].split(b","))
