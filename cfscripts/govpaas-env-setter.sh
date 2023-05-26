882b26f28c1f775b44338a91493bef93558211d2
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2023-05-26 19:11:56 +0100

    Restage apps after env updates

--- cfscripts/govpaas-env-setter.sh
+++ cfscripts/govpaas-env-setter.sh
@@ -25,6 +25,9 @@ for APP in $(echo "${APPS}" | jq -r 'keys | .[]'); do
     # Get the parameters for the app
     PARAMS=$(echo "${APPS}" | jq -r ".\"$APP\" | keys[]")
     
+    # Variable to track if any changes were made
+    CHANGES_MADE=0
+    
     # Loop through the parameters
     for PARAM in $PARAMS; do
         # Get the value from the JSON file
@@ -43,6 +46,15 @@ for APP in $(echo "${APPS}" | jq -r 'keys | .[]'); do
         echo "Setting $PARAM for ${APP}-${CF_SPACE} in $CF_SPACE to $VALUE"
         # cf set-env $APP $PARAM $VALUE &> /dev/null
         ./cf set-env ${APP}-${CF_SPACE} $PARAM $VALUE &> /dev/null
+        if [ $? -eq 0 ]; then
+            # Set CHANGES_MADE to 1 if the set-env command was successful
+            CHANGES_MADE=1
+        fi
     done
-done
 
+    # If any changes were made, restage the app
+    if [ $CHANGES_MADE -eq 1 ]; then
+        echo "Restaging ${APP}-${CF_SPACE} in $CF_SPACE"
+        ./cf restage ${APP}-${CF_SPACE} &> /dev/null
+    fi
+done
