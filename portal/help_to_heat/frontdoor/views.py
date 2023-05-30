d5c5ac7ba5eebec2156692b34077b76073450d9b
Ed Singleton <singletoned@gmail.com>
2023-05-30 08:24:06 +0100

    Rename variable

--- portal/help_to_heat/frontdoor/views.py
+++ portal/help_to_heat/frontdoor/views.py
@@ -9,7 +9,7 @@ from . import interface, schemas
 page_map = {}
 
 
-page_field_map = {
+page_compulsory_field_map = {
     "country": ("country",),
     "own-property": ("own_property",),
     "address": ("address_line_1", "postcode"),
@@ -126,7 +126,7 @@ class PageView(utils.MethodDispatcher):
         return redirect("frontdoor:page", session_id=session_id, page_name=next_page_name)
 
     def validate(self, request, session_id, page_name, data, is_change_page):
-        fields = page_field_map.get(page_name, ())
+        fields = page_compulsory_field_map.get(page_name, ())
         missing_fields = tuple(field for field in fields if not data.get(field))
         errors = {field: "Please answer this question" for field in missing_fields}
         return errors
