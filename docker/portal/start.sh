a852f285f0102938b4d9c478ebf0beb61d8db1d2
Ed Singleton <singletoned@gmail.com>
2023-05-30 15:08:33 +0100

    Add info to start script

--- docker/portal/start.sh
+++ docker/portal/start.sh
@@ -5,6 +5,12 @@ set -o nounset
 
 python manage.py migrate --noinput
 
+echo "Migrations completed"
+
 python manage.py add_suppliers
+
+echo "Suppliers check completed"
+
+ech "Starting app"
 
 watchmedo auto-restart --directory=./  --pattern=""*.py"" --recursive -- waitress-serve --port=$PORT --threads=8 help_to_heat.wsgi:application
