dabab66b77b88f3bb7ec8a7a6033b6c5827c4096
Ed Singleton <singletoned@gmail.com>
2023-06-06 09:01:11 +0100

    Fix decorators

--- portal/help_to_heat/portal/decorators.py
+++ portal/help_to_heat/portal/decorators.py
@@ -1,22 +1,26 @@
 from django.contrib.auth.decorators import user_passes_test
 
-
-def requires_service_manager(func):
-    return user_passes_test(
-        lambda user: user.is_authenticated and user.is_supplier_admin and user.is_active,
-        login_url="portal:unauthorised",
-    )
+requires_service_manager = user_passes_test(
+    lambda user: (user.is_authenticated and user.is_supplier_admin and user.is_active),
+    login_url="portal:unauthorised",
+)
 
 
-def requires_team_leader(func):
-    return user_passes_test(
-        lambda user: user.is_authenticated and user.is_team_leader and user.supplier and user.is_active,
-        login_url="portal:unauthorised",
-    )
+requires_team_leader = user_passes_test(
+    lambda user: (user.is_authenticated and user.is_team_leader and user.supplier and user.is_active),
+    login_url="portal:unauthorised",
+)
 
 
-def requires_team_member(func):
-    return user_passes_test(
-        lambda user: user.is_authenticated and user.is_team_member and user.supplier and user.is_active,
-        login_url="portal:unauthorised",
-    )
+requires_team_member = user_passes_test(
+    lambda user: (user.is_authenticated and user.is_team_member and user.supplier and user.is_active),
+    login_url="portal:unauthorised",
+)
+
+
+requires_team_leader_or_member = user_passes_test(
+    lambda user: (
+        user.is_authenticated and (user.is_team_leader or user.is_team_member) and user.supplier and user.is_active
+    ),
+    login_url="portal:unauthorised",
+)
