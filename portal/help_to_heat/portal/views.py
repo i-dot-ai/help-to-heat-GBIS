7ff8630bca4e254a78bb5620236b14dd1cea3445
Ed Singleton <singletoned@gmail.com>
2023-06-06 09:46:53 +0100

    Refactor portal homepage_view

--- portal/help_to_heat/portal/views.py
+++ portal/help_to_heat/portal/views.py
@@ -3,12 +3,10 @@ from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.utils import timezone
-from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
 
-from . import models
+from . import decorators, models
 
 
 @require_http_methods(["GET"])
@@ -30,37 +28,62 @@ def unauthorised_view(request):
 @login_required(login_url="portal:unauthorised")
 def homepage_view(request):
     user = request.user
-    if not user.is_supplier_admin and not user.is_team_leader and not user.is_team_member:
-        return redirect("portal:unauthorised")
-    if (user.is_team_leader or user.is_team_member) and not user.supplier:
-        return unauthorised_view(request)
-    template = "unauthorised"
-    data = {}
-    if user.is_team_member:
-        template = "portal/team-member/homepage.html"
-    if user.is_team_leader:
-        supplier = user.supplier
-        team_members = models.User.objects.filter(
-            Q(supplier=supplier) & (Q(is_team_member=True) | Q(is_team_leader=True))
-        )
-
-        data = {
-            "team_members": team_members,
-        }
-        template = "portal/team-leader/homepage.html"
-    if user.is_team_member or user.is_team_leader:
-        supplier = user.supplier
-        referrals = models.Referral.objects.filter(referral_download=None, supplier=supplier)
-        unread_leads = referrals.count()
-        archives = models.ReferralDownload.objects.filter(referral_download__supplier=supplier).order_by("-created_at")
-        data = {"supplier": supplier, "unread_leads": unread_leads, "archives": archives, **data}
     if user.is_supplier_admin:
-        template = "portal/supplier-admin/homepage.html"
-        suppliers = models.Supplier.objects.all()
+        return service_manager_homepage_view(request)
+    elif user.is_team_leader:
+        return team_leader_homepage_view(request)
+    elif user.is_team_member:
+        return team_member_homepage_view(request)
+    else:
+        return redirect("portal:unauthorised")
 
-        data = {
-            "suppliers": suppliers,
-        }
+
+@require_http_methods(["GET"])
+@decorators.requires_service_manager
+def service_manager_homepage_view(request):
+    template = "portal/supplier-admin/homepage.html"
+    suppliers = models.Supplier.objects.all()
+    data = {
+        "suppliers": suppliers,
+    }
+    return render(
+        request,
+        template_name=template,
+        context={"request": request, "data": data},
+    )
+
+
+@require_http_methods(["GET"])
+@decorators.requires_team_leader
+def team_leader_homepage_view(request):
+    template = "portal/team-leader/homepage.html"
+    user = request.user
+    supplier = user.supplier
+    team_members = models.User.objects.filter(Q(supplier=supplier) & (Q(is_team_member=True) | Q(is_team_leader=True)))
+    data = {
+        "team_members": team_members,
+    }
+    referrals = models.Referral.objects.filter(referral_download=None, supplier=supplier)
+    unread_leads = referrals.count()
+    archives = models.ReferralDownload.objects.filter(referral_download__supplier=supplier).order_by("-created_at")
+    data = {"supplier": supplier, "unread_leads": unread_leads, "archives": archives, **data}
+    return render(
+        request,
+        template_name=template,
+        context={"request": request, "data": data},
+    )
+
+
+@require_http_methods(["GET"])
+@decorators.requires_team_member
+def team_member_homepage_view(request):
+    template = "portal/team-member/homepage.html"
+    user = request.user
+    supplier = user.supplier
+    referrals = models.Referral.objects.filter(referral_download=None, supplier=supplier)
+    unread_leads = referrals.count()
+    archives = models.ReferralDownload.objects.filter(referral_download__supplier=supplier).order_by("-created_at")
+    data = {"supplier": supplier, "unread_leads": unread_leads, "archives": archives}
     return render(
         request,
         template_name=template,
