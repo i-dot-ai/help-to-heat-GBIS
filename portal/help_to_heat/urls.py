67daffdffed565679efe4db7069d3d278b70ac38
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-06-12 10:55:31 +0100

    Sorting

--- portal/help_to_heat/urls.py
+++ portal/help_to_heat/urls.py
@@ -5,7 +5,6 @@ from help_to_heat import views
 from help_to_heat.frontdoor.urls import frontdoor_patterns
 from help_to_heat.portal.urls import api_patterns, portal_patterns
 
-
 if not settings.KILL_SWITCH:
     if settings.SHOW_FRONTDOOR:
         urlpatterns = [
