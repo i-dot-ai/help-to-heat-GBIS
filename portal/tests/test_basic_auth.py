7dac7f40490ac9db89fd2246ca0348933c175cbe
Ed Singleton <singletoned@gmail.com>
2023-06-13 11:55:07 +0100

    Add portal basic_auth test

--- portal/tests/test_basic_auth.py
+++ portal/tests/test_basic_auth.py
@@ -10,3 +10,10 @@ def test_homepage_basic_auth():
     client = utils.get_client()
     page = client.get("/")
     assert page.status_code == 401
+
+
+@unittest.skipIf(not settings.BASIC_AUTH, "No basic auth set")
+def test_portal_basic_auth():
+    client = utils.get_client()
+    page = client.get("/portal/")
+    assert page.status_code == 401
