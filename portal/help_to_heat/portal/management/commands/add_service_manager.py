a9a7c697c10c5283650e30b886e4565adccd7800
Ed Singleton <singletoned@gmail.com>
2023-06-22 14:25:48 +0100

    Add ultra simple script

---
+++ portal/help_to_heat/portal/management/commands/add_service_manager.py
@@ -0,0 +1,19 @@
+from django.utils import timezone
+from django.core.management.base import BaseCommand
+
+from help_to_heat.portal import models
+
+
+class Command(BaseCommand):
+    help = "Add a service manager to the database"
+
+    def add_arguments(self, parser):
+        parser.add_argument("-e", "--email", type=str, help="The email of the user")
+
+    def handle(self, *args, **kwargs):
+        email = kwargs["email"]
+        user = models.User(email=email)
+        user.is_supplier_admin = True
+        user.invited_at = timezone.now()
+        user.invite_accepted_at = timezone.now()
+        user.save()
