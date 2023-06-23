fa50612026cc6b38f086ee136d2cc8fd67a9628d
Antonio Lopes <i.ai@wdlog.com>
2023-06-23 14:50:24 +0100

    Cookie banner updates

--- static/main.js
+++ static/main.js
@@ -3,3 +3,24 @@ document.body.className = document.body.className
   : "js-enabled";
 
 window.GOVUKFrontend.initAll();
+
+window.onload = () => {
+  const hideButton = document.getElementById("hideButton");
+  const cookieBanner = document.getElementById("cookie-banner");
+
+  const isCookieSet = document.cookie
+    .split(";")
+    .some((item) => item.trim().startsWith("hideButton="));
+
+  if (isCookieSet) cookieBanner.style.display = "none";
+
+  hideButton.addEventListener("click", () => {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + 1);
+
+    document.cookie =
+      "hideButton=hidden; expires=" + date.toUTCString() + "; path=/";
+
+    cookieBanner.style.display = "none";
+  });
+};
