354e1677ac3b406e799d0aa61c2088242ffa4686
Ed Singleton <singletoned@gmail.com>
2023-06-30 12:41:50 +0100

    Add s to url

--- help_to_heat/portal/urls.py
+++ help_to_heat/portal/urls.py
@@ -68,7 +68,7 @@ portal_patterns = [
     ),
     path("data-download/", download_views.download_csv_view, name="data-download"),
     path("data-download/<uuid:download_id>/", download_views.download_csv_by_id_view, name="download-csv-by-id"),
-    path("epc-upload/", views.EPCUploadView, name="epc-upload"),
+    path("epc-uploads/", views.epc_page, name="epc-uploads"),
     path("accounts/password-reset/", authentication_views.PasswordReset, name="password-reset"),
     path("accounts/change-password/reset/", authentication_views.PasswordChange, name="password-reset-change"),
     path("accounts/password-reset-done/", authentication_views.password_reset_done, name="password-reset-done"),
